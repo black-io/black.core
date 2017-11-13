892c7b69e74ecc2a14abaeeb7625f29cfed0c81c
Frank Stein <dr.frank.stain@gmail.com>
2017-11-13 23:49:32 +0300

    More type traits.

--- include/type-traits/std-types.h
+++ include/type-traits/std-types.h
@@ -8,5 +8,13 @@ inline namespace Core
 	// Underlying enumeration type.
 	template< typename TEnumeration >
 	using UnderlyingType = typename std::underlying_type<TEnumeration>::type;
+
+	// Conditional type.
+	template< bool CONDITION, typename TTrueType, typename TFalseType >
+	using Conditional = typename std::conditional<CONDITION, TTrueType, TFalseType>::type;
+
+	// Conditional type switch.
+	template< bool CONDITION, typename TTrueType = void >
+	using EnableIf = typename std::enable_if<CONDITION, TTrueType>::type;
 }
 }
