89fac39e8d72d5533e795f5e543e880d43bc9ccb
Frank Stein <dr.frank.stain@gmail.com>
2017-11-20 23:26:58 +0300

    Intrusive hook interface.

--- include/types/IntrusiveHook.h
+++ include/types/IntrusiveHook.h
@@ -25,7 +25,35 @@ inline namespace Types
 			virtual void InvalidateIntrusiveLink() = 0;
 		};
 
+	// Public interface.
+	public:
+		// Cares about proper invalidation of hook.
+		virtual ~IntrusiveHook();
+
+
+		// Add the link into hook.
+		// This is a service function, it may be used only by intrusive link.
+		void AddIntrusiveLink( Black::NotNull<RegularIntrusiveLink> link );
+
+		// Remove the link form hook.
+		// This is a service function, it may be used only by intrusive link.
+		void RemoveIntrusiveLink( Black::NotNull<RegularIntrusiveLink> link );
+
+	// Construction interface.
+	protected:
+		IntrusiveHook() = default;
+		IntrusiveHook( const IntrusiveHook& other );
+		IntrusiveHook( IntrusiveHook&& other );
+
+		IntrusiveHook& operator = ( const IntrusiveHook& other );
+		IntrusiveHook& operator = ( IntrusiveHook&& other );
+
 	private:
+		// Invalidate the hook.
+		void Invalidate();
+
+	private:
+		std::deque<RegularIntrusiveLink*>	m_liinks;	// Connected links.
 	};
 }
 }
