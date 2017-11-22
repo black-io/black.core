ec556b18678ec1fc5208973b028d320b20569767
Frank Stein <dr.frank.stain@gmail.com>
2017-11-23 00:08:27 +0300

    Win desktop definitions for platform-specific utils.

--- source/platform.windows/platform-specific-utils.windows.cpp
+++ source/platform.windows/platform-specific-utils.windows.cpp
@@ -9,15 +9,45 @@ inline namespace Utils
 {
 inline namespace PlatformSpecific
 {
+	void CopyMemory( void* dest_memory, const void* source_memory, const size_t length )
+	{
+		::memcpy_s( dest_memory, length, source_memory, length );
+	}
 
+	void FillMemory( void* memory, const int32_t pattern, const size_t length )
+	{
+		::memset( memory, pattern, length );
+	}
+
+	void SequredZeroMemory( void* memory, const size_t length )
+	{
+		::RtlSecureZeroMemory( memory, length );
+	}
+
+	const bool IsMemoryEqual( const void* left_memory, const void* right_memory, const size_t length )
+	{
+		return ::memcmp( left_memory, right_memory, length ) == 0;
+	}
 }
 }
+
 
 inline namespace TextUtils
 {
 inline namespace PlatformSpecific
 {
+	Black::StringView WriteArguments( Black::PlainView<char> target_buffer, const Black::StringView format, va_list arguments )
+	{
+		const int32_t status = ::vsprintf_s( target_buffer.GetData(), target_buffer.GetLength(), format.data(), arguments );
+		CRET( status <= 0, {} );
 
+		return { target_buffer.GetData(), static_cast<size_t>( status ) };
+	}
+
+	const size_t ReadArguments( const Black::StringView source_buffer, const Black::StringView format, va_list arguments )
+	{
+		return std::max<int32_t>( ::vsscanf_s( source_buffer.data(), format.data(), arguments ), 0 );
+	}
 }
 }
 }
