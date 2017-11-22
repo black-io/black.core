28b7edc4e6dd2da68b2d4782a67443cc70582f03
Frank Stein <dr.frank.stain@gmail.com>
2017-11-22 23:36:50 +0300

    Basic platform-dependent code for Win desktop platform.

---
+++ include/platform.windows/platform.windows.h
@@ -0,0 +1,67 @@
+#pragma once
+
+
+#include <windows.h>
+
+// Release worthless macros
+
+#if( defined( GetCurrentTime ) )
+	#undef GetCurrentTime
+#endif
+
+#if( defined( min ) )
+	#undef min
+#endif
+
+#if( defined( max ) )
+	#undef max
+#endif
+
+#if( defined( CopyMemory ) )
+	#undef CopyMemory
+#endif
+
+#if( defined( FillMemory ) )
+	#undef FillMemory
+#endif
+
+#if( defined( MoveMemory ) )
+	#undef MoveMemory
+#endif
+
+#if( defined( ZeroMemory ) )
+	#undef ZeroMemory
+#endif
+
+#if( defined( ERROR ) )
+	#undef ERROR
+#endif
+
+#if( defined( CopyFile ) )
+	#undef CopyFile
+#endif
+
+#if( defined( MoveFile ) )
+	#undef MoveFile
+#endif
+
+#if( defined( DeleteFile ) )
+	#undef DeleteFile
+#endif
+
+#if( defined( AddMonitor ) )
+	#undef AddMonitor
+#endif
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace Platform
+{
+inline namespace WindowsDesktop
+{}
+}
+}
+}
