53202840f9657fb51ed6c7bcd95f6472a44abd9f
Frank Stein <dr.frank.stain@gmail.com>
2017-12-05 00:17:45 +0300

    More appropriate interface for unreliable hash function.

--- include/utils/hash-utils.h
+++ include/utils/hash-utils.h
@@ -16,27 +16,7 @@ inline namespace HashUtils
 		@param	seed	Initial value of hash.
 		@return			The value returned is an has of given data, considering the given seed.
 	*/
-	inline const uint32_t GetUnreliableHash( Black::PlainView<const uint8_t> data, const uint32_t seed = 0 );
-
-	/**
-		@brief	Hash the given data using reliable function.
-		Results of reliable hash functions may be freely used over application, including the file operations and network interactions.
-		@note			'Reliable' means here that the result of hash function depends only on input data and seed.
-		@param	data	The data stream to be hashed.
-		@param	seed	Initial value of hash.
-		@return			The value returned is an has of given data, considering the given seed.
-	*/
-	//const uint32_t GetReliableHash( Black::PlainView<const uint8_t> data, const uint32_t seed = 0 );
-
-	/**
-		@brief	Hash the given data using reliable function.
-		Results of reliable hash functions may be freely used over application, including the file operations and network interactions.
-		@note			'Reliable' means here that the result of hash function depends only on input data and seed.
-		@param	data	The data stream to be hashed.
-		@param	seed	Initial value of hash.
-		@return			The value returned is an has of given data, considering the given seed.
-	*/
-	//const uint64_t GetReliableLongHash( Black::PlainView<const uint8_t> data, const uint64_t seed = 0 );
+	inline const uint32_t GetUnreliableHash( const void* const memory, const size_t length, const uint32_t seed = 0 );
 }
 }
 }
