d52edb5f808b82b97ee1ec970616c12b6812de71
Frank Stein <dr.frank.stain@gmail.com>
2017-12-09 20:53:46 +0300

    New interface for static strings.

--- include/types/StaticString.h
+++ include/types/StaticString.h
@@ -7,56 +7,32 @@ inline namespace Core
 {
 inline namespace Types
 {
-	// Compile-time string implementation.
+	/**
+		@brief	Compile-time string implementation.
+		Static (at compile-time) string is an regular type, which may be used to get the regular string literal for runtime purposes.
+		Also, the proper string utils may be used to combine or comparing the CT-strings.
+	*/
 	template< char... CHARS >
 	struct StaticString final
 	{
-		// Get the regular string.
-		constexpr const char* GetData() const;
-
 		// Get the length of string.
-		constexpr const size_t GetLength() const;
+		constexpr static const size_t GetLength()
+		{
+			return sizeof...( CHARS );
+		}
 
-		// Get the element at position.
-		constexpr char GetElement( const size_t index ) const;
+		// Get the regular string.
+		inline static const char* const GetData()
+		{
+			constexpr static const char data[] = { CHARS..., 0 };
+			return data;
+		}
 
-		// Concatenate two strings.
-		template< char... OTHER_CHARS >
-		constexpr auto Combine( StaticString<OTHER_CHARS...> other ) const;
-
-		// Remove the prefix from string.
-		template< size_t PREFIX_LENGTH >
-		constexpr auto RemovePrefix() const;
-
-		// Remove the suffix from string.
-		template< size_t PREFIX_LENGTH >
-		constexpr auto RemoveSuffix() const;
-
-		// Extract the substring from string.
-		template< size_t POSITION, size_t LENGTH = Black::UNDEFINED_INDEX >
-		constexpr auto GetSubstring() const;
-
-		// Compare two strings.
-		template< char... OTHER_CHARS >
-		constexpr bool IsEqual( StaticString<OTHER_CHARS...> other ) const;
-
-		// Check that the string starts with given substring.
-		template< char... OTHER_CHARS >
-		constexpr bool IsStartsWIth( StaticString<OTHER_CHARS...> other ) const;
-
-		// Check that the string ends with given substring.
-		template< char... OTHER_CHARS >
-		constexpr bool IsEndsWIth( StaticString<OTHER_CHARS...> other ) const;
-
-
-		template< char... OTHER_CHARS >
-		constexpr auto operator + ( StaticString<OTHER_CHARS...> other ) const	{ return Combine( other ); };
-
-		template< char... OTHER_CHARS >
-		constexpr bool operator == ( StaticString<OTHER_CHARS...> other ) const	{ return IsEqual( other ); };
-
-		template< char... OTHER_CHARS >
-		constexpr bool operator != ( StaticString<OTHER_CHARS...> other ) const	{ return !IsEqual( other ); };
+		// Get the char at given index. Return `0` for any invalid index.
+		inline static const char GetChar( const size_t index )
+		{
+			return ( index < GetLength() )? GetData()[ index ] : 0;
+		}
 	};
 }
 }
