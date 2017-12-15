75ed400385ff73b6df303dcaf80cc3be94e1ae38
Frank Stein <dr.frank.stain@gmail.com>
2017-12-15 10:18:23 +0300

    First code for substring extractor.

--- include/type-traits/StaticStrings.Substring.h
+++ include/type-traits/StaticStrings.Substring.h
@@ -11,13 +11,34 @@ namespace StaticStrings
 {
 namespace Traits
 {
+	//
+	template< size_t POSITION, size_t LENGTH, typename TString >
+	struct Substring;
 
+	//
+	template< size_t POSITION, size_t LENGTH, char... CHARS >
+	struct Substring< POSITION, LENGTH, Black::StaticString<CHARS...> >
+	{
+		//
+		using UnprefixedString = typename Bisect<POSITION, false, Black::StaticString<>, Black::StaticString<CHARS...>>::RightString;
+
+		//
+		using String = typename Bisect<LENGTH, LENGTH == 0, Black::StaticString<>, UnprefixedString>::LeftString;
+	};
+
+	//
+	template< size_t LENGTH, char... CHARS >
+	struct Substring< 0, LENGTH, Black::StaticString<CHARS...> >
+	{
+		//
+		using String = typename Bisect<LENGTH, LENGTH == 0, Black::StaticString<>, Black::StaticString<CHARS...>>::LeftString;
+	};
 }
 
 
 	//
 	template< size_t POSITION, size_t LENGTH, typename TString >
-	using Substring = void;
+	using Substring = typename Traits::Substring<POSITION, LENGTH, TString>::String;
 }
 }
 }
