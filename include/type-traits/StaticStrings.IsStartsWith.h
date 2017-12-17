fc49102ff2875cf242a7fe42af8103a90d4d74ca
Frank Stein <dr.frank.stain@gmail.com>
2017-12-17 23:00:01 +0300

    Comments.

--- include/type-traits/StaticStrings.IsStartsWith.h
+++ include/type-traits/StaticStrings.IsStartsWith.h
@@ -11,45 +11,45 @@ namespace StaticStrings
 {
 namespace Traits
 {
-	//
+	// Test for string prefix check.
 	template< typename TString, typename TPrefixString >
 	struct IsStartsWith;
 
-	//
+	// The way to shortcut the empty strings.
 	template<>
 	struct IsStartsWith<Black::StaticString<>, Black::StaticString<>> : std::false_type
 	{
 
 	};
 
-	//
+	// The way to shortcut the empty pattern.
 	template< char... CHARS >
 	struct IsStartsWith<Black::StaticString<CHARS...>, Black::StaticString<>> : std::false_type
 	{
 
 	};
 
-	//
+	// The way to shortcut the empty string.
 	template< char... CHARS >
 	struct IsStartsWith<Black::StaticString<>, Black::StaticString<CHARS...>> : std::false_type
 	{
 
 	};
 
-	//
+	// The way to check that the string prefix is equal to pattern.
 	template< char... STRING_CHARS, char... PATTERN_CHARS >
 	struct IsStartsWith<Black::StaticString<STRING_CHARS...>, Black::StaticString<PATTERN_CHARS...>>
 	{
-		//
+		// Cut off the string suffix.
 		using StringPrefix = typename Bisect<sizeof...( PATTERN_CHARS ), false, Black::StaticString<>, Black::StaticString<STRING_CHARS...>>::LeftString;
 
-		//
+		// Test the equality of string prefix with pattern.
 		constexpr static const bool value = IsEqual<Black::StaticString<PATTERN_CHARS...>, StringPrefix>::value;
 	};
 }
 
 
-	//
+	// Check that the static string starts with given pattern.
 	template< typename TString, typename TPrefixString >
 	constexpr bool IS_STARTS_WITH = Traits::IsStartsWith<TString, TPrefixString>::value;
 }
