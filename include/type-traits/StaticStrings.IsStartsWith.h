906e4926b337c68afa7a4c511b60e11a16c0bf7d
Frank Stein <dr.frank.stain@gmail.com>
2017-12-17 20:04:54 +0300

    Predicate for static strings to check that the string starts with given pattern.

--- include/type-traits/StaticStrings.IsStartsWith.h
+++ include/type-traits/StaticStrings.IsStartsWith.h
@@ -11,8 +11,47 @@ namespace StaticStrings
 {
 namespace Traits
 {
+	//
+	template< typename TString, typename TPrefixString >
+	struct IsStartsWith;
 
+	//
+	template<>
+	struct IsStartsWith<Black::StaticString<>, Black::StaticString<>> : std::false_type
+	{
+
+	};
+
+	//
+	template< char... CHARS >
+	struct IsStartsWith<Black::StaticString<CHARS...>, Black::StaticString<>> : std::false_type
+	{
+
+	};
+
+	//
+	template< char... CHARS >
+	struct IsStartsWith<Black::StaticString<>, Black::StaticString<CHARS...>> : std::false_type
+	{
+
+	};
+
+	//
+	template< char... STRING_CHARS, char... PATTERN_CHARS >
+	struct IsStartsWith<Black::StaticString<STRING_CHARS...>, Black::StaticString<PATTERN_CHARS...>>
+	{
+		//
+		using StringPrefix = typename Bisect<sizeof...( PATTERN_CHARS ), false, Black::StaticString<>, Black::StaticString<STRING_CHARS...>>::LeftString;
+
+		//
+		constexpr static const bool value = IsEqual<Black::StaticString<PATTERN_CHARS...>, StringPrefix>::value;
+	};
 }
+
+
+	//
+	template< typename TString, typename TPrefixString >
+	constexpr bool IS_STARTS_WITH = Traits::IsStartsWith<TString, TPrefixString>::value;
 }
 }
 }
