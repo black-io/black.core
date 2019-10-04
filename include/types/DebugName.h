3fb61fd6528db1249aab272f1838de8f3f47d75d
Frank Stein <dr.frank.stain@gmail.com>
2019-10-04 08:52:15 +0300

    Refactoring of debug name.

--- include/types/DebugName.h
+++ include/types/DebugName.h
@@ -9,57 +9,70 @@ inline namespace Types
 {
 namespace Internal
 {
-	// Default implementation of debug name.
+	// Common deduction branch. True functionality.
 	template< Black::BuildMode >
 	class DebugName final
 	{
-	public:
-		DebugName()											= default;
-		DebugName( const DebugName& )						= default;
-		DebugName( DebugName&& )							= default;
-		DebugName( ... )									{};
-
-
-		// Fake name getter for non-debug configurations.
-		constexpr const char* const GetName() const			{ return "n/a"; };
-
-
-		inline DebugName& operator = ( const DebugName& )	= default;
-		inline DebugName& operator = ( DebugName&& )		= default;
-		inline DebugName& operator = ( std::string&& )		{ return *this; };
-		inline DebugName& operator = ( Black::StringView )	{ return *this; };
-		inline DebugName& operator = ( const char* const )	{ return *this; };
-
-		constexpr const char* const operator * () const		{ return GetName(); };
-	};
-
-	// Explicit specialization for Debug name in Debug build configuration.
-	template<>
-	class DebugName<Black::BuildMode::Debug> final
-	{
+	// Construction and initialization.
 	public:
 		DebugName()						= default;
 		DebugName( const DebugName& )	= default;
 		DebugName( DebugName&& )		= default;
-		DebugName( std::string&& name ) : m_name{ std::move( name ) } {};
+		DebugName( std::string name ) : m_name{ std::move( name ) } {};
+		DebugName( std::string_view name ) : m_name{ name } {};
 		DebugName( Black::StringView name ) : m_name{ name.begin(), name.end() } {};
 		DebugName( const char* const name ) : m_name{ name } {};
 
 
+		inline DebugName& operator = ( const DebugName& )		= default;
+		inline DebugName& operator = ( DebugName&& )			= default;
+		inline DebugName& operator = ( std::string name )		{ return Black::CopyAndSwap( *this, std::move( name ) ); };
+		inline DebugName& operator = ( std::string_view name )	{ return Black::CopyAndSwap( *this, name ); };
+		inline DebugName& operator = ( Black::StringView name )	{ return Black::CopyAndSwap( *this, name ); };
+		inline DebugName& operator = ( const char* const name )	{ return Black::CopyAndSwap( *this, name ); };
+
+	// Public interface.
+	public:
 		// Get the name.
 		inline const char* const GetName() const				{ return m_name.data(); };
 
 
-		inline DebugName& operator = ( const DebugName& )		= default;
-		inline DebugName& operator = ( DebugName&& )			= default;
-		inline DebugName& operator = ( std::string&& name )		{ m_name = std::move( name ); return *this; };
-		inline DebugName& operator = ( Black::StringView name )	{ m_name = { name.begin(), name.end() }; return *this; };
-		inline DebugName& operator = ( const char* const name )	{ m_name = name; return *this; };
-
 		inline const char* const operator * () const			{ return GetName(); };
 
+	// Private state.
 	private:
 		std::string	m_name;	// Stored name.
+	};
+
+	// Terminal deduction branch. Fake functionality for release build.
+	template<>
+	class DebugName<Black::BuildMode::Release> final
+	{
+	// Fake initialization.
+	public:
+		DebugName()												= default;
+		DebugName( const DebugName& )							= default;
+		DebugName( DebugName&& )								= default;
+		DebugName( std::string name )							{};
+		DebugName( std::string_view name )						{};
+		DebugName( Black::StringView name )						{};
+		DebugName( const char* const name )						{};
+
+
+		inline DebugName& operator = ( const DebugName& )		= default;
+		inline DebugName& operator = ( DebugName&& )			= default;
+		inline DebugName& operator = ( std::string name )		{ return *this; };
+		inline DebugName& operator = ( std::string_view name )	{ return *this; };
+		inline DebugName& operator = ( Black::StringView name )	{ return *this; };
+		inline DebugName& operator = ( const char* const name )	{ return *this; };
+
+	// Fake interface.
+	public:
+		// Fake name getter.
+		inline const char* const GetName() const				{ return ""; };
+
+
+		inline const char* const operator * () const			{ return GetName(); };
 	};
 }
 
