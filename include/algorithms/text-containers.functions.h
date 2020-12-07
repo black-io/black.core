14d8dece224df5cea4f1dbe9b7bd384c433b21eb
Frank Stein <dr.frank.stain@gmail.com>
2020-12-07 23:05:14 +0300

    New inline variable used.

--- include/algorithms/text-containers.functions.h
+++ include/algorithms/text-containers.functions.h
@@ -11,7 +11,7 @@ namespace Internal
 {
 	// Helper to determine the length of unknown string type. This branch helps to know the length of single char.
 	template< typename TCandidate >
-	inline auto GetStringLength( const TCandidate& candidate ) -> std::enable_if_t<ValidCharTest<TCandidate>::value, const size_t>
+	inline auto GetStringLength( const TCandidate& candidate ) -> std::enable_if_t<IS_VALID_CHAR<TCandidate>, const size_t>
 	{
 		return 1;
 	}
