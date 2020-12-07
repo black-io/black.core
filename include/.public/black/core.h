279ffe4e48b1ec5c57c2f608d0b63be6fd05f34b
Frank Stein <dr.frank.stain@gmail.com>
2020-12-07 15:09:56 +0300

    Public stuff for algorithms.

--- include/.public/black/core.h
+++ include/.public/black/core.h
@@ -7,6 +7,7 @@
 #include "../../concurrency/concurrency.h"
 #include "../../bit-views/bit-views.h"
 #include "../../static-lists/static-lists.h"
+#include "../../algorithms/algorithms.h"
 
 // The epilogue of library.
 #include "../../.epilogue.h"
---
+++ include/.public/black/core/algorithms.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../../.prologue.h"
+
+#include "../../../algorithms/algorithms.h"
+
+// The epilogue of library.
+#include "../../../.epilogue.h"
