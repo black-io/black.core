510304eae6501db08a3d6188ca68c29c6929105c
Frank Stein <dr.frank.stain@gmail.com>
2020-12-08 17:10:43 +0300

    Folder renamed. Global is not a subsystem, but only service folder.

--- include/.epilogue.h
+++ include/.epilogue.h
@@ -2,9 +2,9 @@
 
 
 // Deferred inline implementations.
-#include "global/types/RingBuffer.inl"
-#include "global/types/IntrusiveLink.inl"
-#include "global/types/PlainVector.inl"
-#include "global/types/PlainView.inl"
+#include ".global/types/RingBuffer.inl"
+#include ".global/types/IntrusiveLink.inl"
+#include ".global/types/PlainVector.inl"
+#include ".global/types/PlainView.inl"
 
-#include "global/algorithms/bytes.inl"
+#include ".global/algorithms/bytes.inl"
