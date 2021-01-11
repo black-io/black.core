2f7b7bce711ff9f7901309a4d91065f968c8b88a
Frank Stein <dr.frank.stain@gmail.com>
2021-01-11 10:37:17 +0300

    Forward declarations.

---
+++ include/static-lists/forwards.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace StaticLists
+{
+	// Forward declaration to properly declare the friendship.
+	template< typename TInterface >
+	class StaticList;
+}
+}
+}
