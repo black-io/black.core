75d878e5e96ac4dc98ec43a8896d58d887ddfd8d
Frank Stein <dr.frank.stain@gmail.com>
2021-01-12 20:08:42 +0300

    Basic static node refactored.

--- include/static-lists/internal/BasicStaticNode.h
+++ include/static-lists/internal/BasicStaticNode.h
@@ -10,24 +10,24 @@ inline namespace StaticLists
 namespace Internal
 {
 	/**
-		@brief	Basic interface for static list nodes.
-		The `StaticList` carries nodes as statically placed (static-duration storage) instances of `BasicStaticNode`.
+		@brief	Basic interface for nodes of particular static collection.
+
+		The `BasicStaticList` carries nodes as statically placed (static-duration storage) instances of `BasicStaticNode`.
 		Each basic node is an element of intrusive forward list.
 
-		On its creation, the basic node stores current list head into `m_next_node` and places itself into list head.
+		On its creation, the basic node may store, if required, current list head into `m_next_node` and place itself into list head.
 		Each static node should be created with its own name for debug purposes.
 
 		The instances of basic node should not be constructed or accessed directly.
 
-		@tparam	TInterface	The common interface of stored items.
+		@tparam	TStorageTag		Tagging type to indicate the unique static collection.
 	*/
-	template< typename TInterface >
+	template< typename TStorageTag >
 	class BasicStaticNode : private Black::NonTransferable
 	{
 	// Friendship.
 	public:
-		friend class StaticListIterator<TInterface>;	// Grant access to `m_next_node` member.
-		friend class Black::StaticList<TInterface>;		// Grant access to `m_next_node` member.
+		friend class BasicStaticList<TStorageTag>;	// Grant access to `m_next_node` member.
 
 	// Construction and destruction.
 	public:
@@ -36,26 +36,24 @@ namespace Internal
 
 	// Public interface.
 	public:
-		// Invalidate (destroy) the stored interface.
+		// Invalidate (destroy) the content of node.
 		virtual void Invalidate() = 0;
 
 
-		// Get the stored interface.
-		virtual TInterface& GetInterface() const = 0;
-
 		// Get the debug name (only for debug purposes).
-		inline std::string_view const GetDebugName() const	{ return *m_name; };
+		inline std::string_view GetDebugName() const { return *m_name; };
 
-
-		inline TInterface* operator -> () const				{ return &GetInterface(); };
+	// Heirs construction.
+	protected:
+		inline BasicStaticNode( Black::DebugName&& name )						: m_name{ std::move( name ) } {};
+		inline BasicStaticNode( Black::DebugName&& name, const InstantInsert )	: m_name{ std::move( name ) } { PlugIntoList(); };
 
 	// Heirs interface.
 	protected:
-		BasicStaticNode( Black::DebugName&& name ) : m_name{ std::move( name ) } { PlugIntoList(); };
+		// Get the next node in static list.
+		inline BasicStaticNode* const GetNextNode() const;
 
-	// Private interface.
-	private:
-		// Plug the node into static list.
+		// Plug the node into static list. May be used only once!
 		inline void PlugIntoList();
 
 	// Private state.
--- include/static-lists/internal/BasicStaticNode.inl
+++ include/static-lists/internal/BasicStaticNode.inl
@@ -10,8 +10,15 @@ inline namespace StaticLists
 namespace Internal
 {
 	template< typename TInterface >
+	inline BasicStaticNode<TInterface>* const BasicStaticNode<TInterface>::GetNextNode() const
+	{
+		return m_next_node;
+	}
+
+	template< typename TInterface >
 	inline void BasicStaticNode<TInterface>::PlugIntoList()
 	{
+		EXPECTS_DEBUG( m_next_node == nullptr );
 		m_next_node = std::exchange( Black::StaticList<TInterface>::GetRootNode(), this );
 	}
 }
