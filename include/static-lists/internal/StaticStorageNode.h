4048292a840f637e36fa3e2a6f07e904d97a99b2
Frank Stein <dr.frank.stain@gmail.com>
2021-01-12 23:38:45 +0300

    Static storage node implemented.

---
+++ include/static-lists/internal/StaticStorageNode.h
@@ -0,0 +1,58 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace StaticLists
+{
+namespace Internal
+{
+	/**
+		@brief	Implementation of static storage node.
+
+		Each node of static storage carries its own object of given implementation type. The memory buffer is placed inside of node.
+		Construction of hosted object always deferred to the moment, when the object is required. Also only default construction is allowed for hosted object.
+
+		@tparam	TStorageTag		Tagging type to indicate the unique static collection.
+		@tparam	TImplementation	Type of hosted object.
+	*/
+	template< typename TStorageTag, typename TImplementation >
+	class alignas( TImplementation ) StaticStorageNode final : private BasicStaticNode<TStorageTag>
+	{
+	// Construction and initialization.
+	public:
+		inline StaticStorageNode()										: StaticStorageNode{ TImplementation::GetDebugName() } {};
+		inline explicit StaticStorageNode( Black::DebugName&& name )	: BasicStaticNode<TStorageTag>{ std::move( name ) } {};
+
+	// Public interface.
+	public:
+		inline explicit operator const bool () const					{ return m_implementation != nullptr; };
+		inline const bool operator ! () const							{ return m_implementation == nullptr; };
+
+
+		inline TImplementation& operator * () const						{ return GetImplementation(); };
+		inline TImplementation* operator -> () const					{ return &GetImplementation(); };
+
+	// Private interface.
+	private:
+		// Construct the implementation.
+		inline TImplementation& ConstructImplementation() const;
+
+		// Get the stored implementation.
+		inline TImplementation& GetImplementation() const;
+
+
+		/// @see	BasicStaticNode::Invalidate
+		void Invalidate() override;
+
+	// Private state.
+	private:
+		mutable	TImplementation*	m_implementation						= nullptr;	// Pointer to allocated instance.
+		mutable std::byte			m_storage[ sizeof( TImplementation ) ];				// Memory buffer to store the allocated instance.
+	};
+}
+}
+}
+}
---
+++ include/static-lists/internal/StaticStorageNode.inl
@@ -0,0 +1,38 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace StaticLists
+{
+namespace Internal
+{
+	template< typename TStorageTag, typename TImplementation >
+	inline TImplementation& StaticStorageNode<TStorageTag, TImplementation>::ConstructImplementation() const
+	{
+		EXPECTS_DEBUG( m_implementation == nullptr );
+		m_implementation = ConstructionProxy<TImplementation>::Construct( m_storage );
+
+		ENSURES_DEBUG( m_implementation != nullptr );
+		BasicStaticNode<TStorageTag>::PlugIntoList();
+		return *m_implementation;
+	}
+
+	template< typename TStorageTag, typename TImplementation >
+	inline TImplementation& StaticStorageNode<TStorageTag, TImplementation>::GetImplementation() const
+	{
+		return ( m_implementation == nullptr )? ConstructImplementation() : *m_implementation;
+	}
+
+	template< typename TStorageTag, typename TImplementation >
+	void StaticStorageNode<TStorageTag, TImplementation>::Invalidate()
+	{
+		CRET( m_implementation == nullptr );
+		std::exchange(m_implementation, nullptr)->~TImplementation();
+	}
+}
+}
+}
+}
