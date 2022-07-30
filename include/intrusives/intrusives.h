49a47072169ccfdc76a127c5027b78bbe2dfc500
Frank Stein <dr.frank.stain@gmail.com>
2022-07-30 15:57:53 +0300

    Master-header of subsystem.

---
+++ include/intrusives/intrusives.h
@@ -0,0 +1,35 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+// Core intrusive objects subsystem.
+inline namespace Intrusives
+{
+// Internal definitions for intrusives.
+namespace Internal
+{
+
+}
+}
+}
+}
+
+
+// Most fundamental stuff.
+#include "internal/forwards.h"
+
+// Internal stuff.
+#include "internal/SinglyLinkedListSlot.h"
+#include "internal/SinglyLinkedListIterator.h"
+#include "internal/SinglyLinkedList.h"
+
+#include "internal/DoublyLinkedListSlot.h"
+#include "internal/DoublyLinkedListIterator.h"
+#include "internal/DoublyLinkedList.h"
+
+// Intrusive containers.
+
+// Deferred implementations.
