c6fcad2310f63d94a1773c8049eee53156a54438
Frank Stein <dr.frank.stain@gmail.com>
2022-07-30 15:59:56 +0300

    Intrusives included into full header of core library.

--- include/.public/black/core.full.h
+++ include/.public/black/core.full.h
@@ -9,6 +9,7 @@
 #include "../../static-lists/static-lists.h"
 #include "../../algorithms/algorithms.h"
 #include "../../static-strings/static-strings.h"
+#include "../../intrusives/intrusives.h"
 
 // The epilogue of library.
 #include "../../.epilogue.h"
