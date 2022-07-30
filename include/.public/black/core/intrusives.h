d305122eca9e62e646d2b99fdf63997bc35aa593
Frank Stein <dr.frank.stain@gmail.com>
2022-07-30 15:58:15 +0300

    Public header for subsystem.

---
+++ include/.public/black/core/intrusives.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+// The prologue of library.
+#include "../../../.prologue.h"
+
+// Core intrusive objects subsystem.
+#include "../../../intrusives/intrusives.h"
+
+// The epilogue of library.
+#include "../../../.epilogue.h"
