97fa885a300f5d1e0fe071dc8f4310a0dd6daaef
Frank Stein <dr.frank.stain@gmail.com>
2022-07-30 15:57:21 +0300

    Some crucial forward declarations.

---
+++ include/intrusives/internal/forwards.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace Intrusives
+{
+namespace Internal
+{
+	// To be used in friendship declarations.
+	class SinglyLinkedListIterator;
+
+	// To be used in friendship declarations.
+	class SinglyLinkedList;
+
+	// To be used in friendship declarations.
+	class DoublyLinkedListIterator;
+
+	// To be used in friendship declarations.
+	class DoublyLinkedList;
+}
+}
+}
+}
