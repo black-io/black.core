c0522b6254363de1cb4a1d61267dc7fed897c0f1
Frank Stein <dr.frank.stain@gmail.com>
2022-08-02 10:40:45 +0300

    Iterators for intrusive list implemented.

---
+++ include/intrusives/internal/ListConstIterator.h
@@ -0,0 +1,66 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace Intrusives
+{
+namespace Internal
+{
+	/**
+	*/
+	template< typename TValue, Black::IntrusiveListSlot TValue::* SLOT_POINTER >
+	class ListConstIterator final
+	{
+	// Friendship declarations.
+	public:
+		// Grant access to private state.
+		friend class Black::IntrusiveList<TValue, SLOT_POINTER>;
+
+		// Grant access to private state and constructor.
+		friend class ListIterator<TValue, SLOT_POINTER>;
+
+	// Lifetime management.
+	public:
+		inline ListConstIterator()								= default;
+		inline ListConstIterator( const ListConstIterator& )	= default;
+		inline ListConstIterator( ListConstIterator& )			= default;
+		inline ~ListConstIterator()								= default;
+
+
+		inline ListConstIterator& operator = ( const ListConstIterator& )	= default;
+		inline ListConstIterator& operator = ( ListConstIterator&& )		= default;
+
+	// Public interface.
+	public:
+		inline ListConstIterator& operator ++ ();
+		inline ListConstIterator operator ++ ( int );
+
+		inline ListConstIterator& operator -- ();
+		inline ListConstIterator operator -- ( int );
+
+		inline const TValue& operator * () const;
+		inline const TValue* operator -> () const;
+
+		inline const bool operator == ( const ListConstIterator& other ) const;
+		inline const bool operator != ( const ListConstIterator& other ) const;
+
+	// Private inner types.
+	private:
+		// Traits for intrusive operations.
+		using Traits = IntrusiveTraits<TValue, Black::IntrusiveForwardListSlot, SLOT_POINTER>;
+
+	// Private lifetime management.
+	private:
+		inline explicit ListConstIterator( DoublyLinkedListIterator iterator );
+
+	// Private state.
+	private:
+		DoublyLinkedListIterator m_iterator; // Regular iterator for inner list type.
+	};
+}
+}
+}
+}
