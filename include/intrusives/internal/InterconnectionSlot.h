f0eed0d273f15e93d44c9fa1e1330583a4aa2393
Frank Stein <dr.frank.stain@gmail.com>
2022-08-05 00:04:13 +0300

    Template of valid interconnection slot.

--- include/intrusives/internal/InterconnectionSlot.h
+++ include/intrusives/internal/InterconnectionSlot.h
@@ -5,69 +5,30 @@ namespace Black
 {
 inline namespace Core
 {
-inline namespace Global
-{
-inline namespace Types
+inline namespace Intrusives
 {
 namespace Internal
 {
-	// Intrusive link.
-	template< typename TLinkedType, bool USE_LINKING >
-	class IntrusiveLink;
-
-	// Positive specification for intrusive link.
-	template< typename TLinkedType >
-	class IntrusiveLink<TLinkedType, true> : protected Black::IntrusiveHook::RegularIntrusiveLink
+	/**
+	*/
+	template< typename THost >
+	class InterconnectionSlot : protected BasicInterconnectionSlot
 	{
+	// Lifetime management.
 	public:
-		IntrusiveLink() = default;
-		IntrusiveLink( const IntrusiveLink& other );
-		IntrusiveLink( IntrusiveLink&& other );
-		explicit IntrusiveLink( const TLinkedType* hook );
-		virtual ~IntrusiveLink();
+		inline InterconnectionSlot() = default;
 
+		inline InterconnectionSlot( const InterconnectionSlot& other );
+		inline InterconnectionSlot( InterconnectionSlot&& other );
+		virtual inline ~InterconnectionSlot();
+
+		explicit inline InterconnectionSlot( const THost& host );
+
+	// Heirs compatible interface.
 	protected:
-		// Link the given intrusive hook.
-		virtual void SetHook( const TLinkedType* hook );
-
-		// Invalidate the link.
-		virtual void InvalidateIntrusiveLink() override;
-
-	private:
-		// Register link for current hook.
-		void RegisterLink();
-
-		// Unregister link for current hook.
-		void UnregisterLink();
-
-	private:
-		const TLinkedType*	m_hook	= nullptr;	// Linked intrusive hook via its derived type.
+		// Perform the interconnection.
+		inline void Interconnect( const THost& host );
 	};
-
-	// Negative specification for intrusive link.
-	template< typename TLinkedType >
-	class IntrusiveLink<TLinkedType, false>
-	{
-	public:
-		IntrusiveLink()									= default;
-		IntrusiveLink( const IntrusiveLink& )			= default;
-		IntrusiveLink( IntrusiveLink&& )				= default;
-		explicit IntrusiveLink( const TLinkedType* )	{};
-		virtual ~IntrusiveLink()						= default;
-
-	protected:
-		// Empty stub for functional symmetry.
-		virtual void SetHook( const TLinkedType* )		{};
-
-		// Empty stub for functional symmetry.
-		virtual void InvalidateIntrusiveLink()			{};
-	};
-}
-
-
-	// Intrusive link.
-	template< typename TLinkedType >
-	using IntrusiveLink = Internal::IntrusiveLink<TLinkedType, std::is_base_of_v<Black::IntrusiveHook, TLinkedType>>;
 }
 }
 }
--- include/intrusives/internal/InterconnectionSlot.inl
+++ include/intrusives/internal/InterconnectionSlot.inl
@@ -5,67 +5,41 @@ namespace Black
 {
 inline namespace Core
 {
-inline namespace Global
-{
-inline namespace Types
+inline namespace Intrusives
 {
 namespace Internal
 {
-	template< typename TLinkedType >
-	IntrusiveLink<TLinkedType, true>::IntrusiveLink( const IntrusiveLink& other )
-		: IntrusiveLink{ other.m_hook }
-	{}
-
-	template< typename TLinkedType >
-	IntrusiveLink<TLinkedType, true>::IntrusiveLink( IntrusiveLink&& other )
-		: m_hook{ other.m_hook }
+	template< typename THost >
+	inline InterconnectionSlot<THost>::InterconnectionSlot( const InterconnectionSlot& other )
+		: BasicInterconnectionSlot{ other }
 	{
-		RegisterLink();
-		other.UnregisterLink();
-		other.InvalidateIntrusiveLink();
 	}
 
-	template< typename TLinkedType >
-	IntrusiveLink<TLinkedType, true>::IntrusiveLink( const TLinkedType* hook )
-		: m_hook{ hook }
+	template< typename THost >
+	inline InterconnectionSlot<THost>::InterconnectionSlot( InterconnectionSlot&& other )
+		: BasicInterconnectionSlot{ std::move( static_cast<BasicInterconnectionSlot&>( other ) ) }
 	{
-		RegisterLink();
+		other.Invalidate();
 	}
 
-	template< typename TLinkedType >
-	IntrusiveLink<TLinkedType, true>::~IntrusiveLink()
+	template< typename THost >
+	inline InterconnectionSlot<THost>::~InterconnectionSlot()
 	{
-		UnregisterLink();
+		Detach();
 	}
 
-	template< typename TLinkedType >
-	void IntrusiveLink<TLinkedType, true>::SetHook( const TLinkedType* hook )
+	template< typename THost >
+	inline InterconnectionSlot<THost>::InterconnectionSlot( const THost& host )
 	{
-		UnregisterLink();
-		m_hook= hook;
-		RegisterLink();
+		AttachTo( host );
 	}
 
-	template< typename TLinkedType >
-	void IntrusiveLink<TLinkedType, true>::InvalidateIntrusiveLink()
+	template< typename THost >
+	inline void InterconnectionSlot<THost>::Interconnect( const THost& host )
 	{
-		m_hook = nullptr;
+		Detach();
+		AttachTo( host );
 	}
-
-	template< typename TLinkedType >
-	void IntrusiveLink<TLinkedType, true>::RegisterLink()
-	{
-		CRET( m_hook == nullptr );
-		m_hook->AddIntrusiveLink( this );
-	}
-
-	template< typename TLinkedType >
-	void IntrusiveLink<TLinkedType, true>::UnregisterLink()
-	{
-		CRET( m_hook == nullptr );
-		m_hook->RemoveIntrusiveLink( this );
-	}
-}
 }
 }
 }
