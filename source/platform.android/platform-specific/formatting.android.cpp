971ea33b15036a886a120727935e3e3b2f1273d7
Frank Stein <dr.frank.stain@gmail.com>
2022-08-09 00:11:01 +0300

    Valid usage of plain view.

--- source/platform.android/platform-specific/formatting.android.cpp
+++ source/platform.android/platform-specific/formatting.android.cpp
@@ -11,10 +11,10 @@ inline namespace PlatformSpecific
 {
 	std::string_view FormatArgumentsList( Black::PlainView<char> target_buffer, const char* format, va_list arguments )
 	{
-		const int32_t status = ::vsnprintf( target_buffer.GetData(), target_buffer.GetLength(), format, arguments );
+		const int32_t status = ::vsnprintf( target_buffer.data(), target_buffer.GetLength(), format, arguments );
 		CRET( status <= 0, {} );
 
-		return { target_buffer.GetData(), static_cast<size_t>( status ) };
+		return { target_buffer.data(), static_cast<size_t>( status ) };
 	}
 
 	const size_t ParseArgumentsList( const std::string_view source_buffer, const char* format, va_list arguments )
