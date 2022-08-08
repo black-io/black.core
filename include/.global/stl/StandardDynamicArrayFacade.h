54dc0d43e4b422f7e03cfe36024030db618ba1e1
Frank Stein <dr.frank.stain@gmail.com>
2022-08-08 14:06:58 +0300

    Host type allows more final look of mixed interface.

--- include/.global/stl/StandardDynamicArrayFacade.h
+++ include/.global/stl/StandardDynamicArrayFacade.h
@@ -11,27 +11,27 @@ inline namespace Stl
 {
 	/**
 	*/
-	template< typename THost, const bool USE_CONST_PROPAGATION = true >
-	class StandardDynamicArrayFacade : public StandardArrayFacade<THost, USE_CONST_PROPAGATION>
+	template< typename THost, typename TBase, const bool USE_CONST_PROPAGATION = true >
+	class StandardDynamicArrayFacade : public StandardArrayFacade<THost, TBase, USE_CONST_PROPAGATION>
 	{
 	// Used inner types.
 	public:
-		using StandardArrayFacade<THost, USE_CONST_PROPAGATION>::size_type;
+		using StandardArrayFacade<THost, TBase, USE_CONST_PROPAGATION>::size_type;
 
 	// Public lifetime management.
 	public:
-		using StandardArrayFacade<THost, USE_CONST_PROPAGATION>::StandardArrayFacade;
+		using StandardArrayFacade<THost, TBase, USE_CONST_PROPAGATION>::StandardArrayFacade;
 
 	// Public interface.
 	public:
 		// Returns the number of elements that the container has currently allocated space for.
-		inline const size_type capacity() const					{ return THost::GetCapacity(); };
+		inline const size_type capacity() const					{ return TBase::GetCapacity(); };
 
 		// Increase the capacity of the container to a value that's greater or equal to `new_capacity`.
-		inline void reserve( const size_type new_capacity )		{ THost::ReserveCapacity( new_capacity ); };
+		inline void reserve( const size_type new_capacity )		{ TBase::ReserveCapacity( new_capacity ); };
 
 		// Resizes the container to contain count elements. Capacity may be modified as well.
-		inline void resize( const size_type new_size )			{ THost::SetLength( new_size ); };
+		inline void resize( const size_type new_size )			{ TBase::SetLength( new_size ); };
 	};
 }
 }
