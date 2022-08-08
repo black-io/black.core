5cb33e1e35c8bef16422d40c45a22327c4ade663
Frank Stein <dr.frank.stain@gmail.com>
2022-08-08 17:49:58 +0300

    Public definitions for intrusive list and forward list.

---
+++ include/intrusives/IntrusiveForwardList.h
@@ -0,0 +1,55 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace Intrusives
+{
+	/**
+	*/
+	template< typename TValue, Black::IntrusiveForwardListSlot TValue::* SLOT_POINTER >
+	class IntrusiveForwardList final
+		: public Black::StandardForwardListFacade<IntrusiveForwardList<TValue, SLOT_POINTER>, Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>>
+	{
+	// Public aliases.
+	public:
+		using typename Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::Iterator;
+		using typename Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::ConstIterator;
+
+
+		using Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::IsEmpty;
+
+	// Friendship interface.
+	public:
+		// 'Range-based for loop' interface, the `begin( __range )` statement.
+		friend inline Iterator begin( IntrusiveForwardList& range )							{ return range.GetBegin(); };
+
+		// 'Range-based for loop' interface, the `end( __range )` statement.
+		friend inline Iterator end( IntrusiveForwardList& range )							{ return range.GetEnd(); };
+
+		// 'Range-based for loop' interface, the `begin( __range )` statement.
+		friend inline ConstIterator begin( const IntrusiveForwardList& range )				{ return range.GetBegin(); };
+
+		// 'Range-based for loop' interface, the `end( __range )` statement.
+		friend inline ConstIterator end( const IntrusiveForwardList& range )				{ return range.GetEnd(); };
+
+		// 'Swap' interface.
+		friend inline void swap( IntrusiveForwardList& left, IntrusiveForwardList& right )	{ left.Swap( right ); };
+
+	// Public lifetime management.
+	public:
+		using Black::StandardForwardListFacade<
+			IntrusiveForwardList<TValue, SLOT_POINTER>,
+			Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>
+		>::StandardForwardListFacade;
+
+	// Public interface.
+	public:
+		inline explicit operator const bool () const	{ return !IsEmpty(); };
+		inline const bool operator ! () const			{ return IsEmpty(); };
+	};
+}
+}
+}
---
+++ include/intrusives/IntrusiveList.h
@@ -0,0 +1,55 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace Intrusives
+{
+	/**
+	*/
+	template< typename TValue, Black::IntrusiveListSlot TValue::* SLOT_POINTER >
+	class IntrusiveList final
+		: public Black::StandardListFacade<IntrusiveList<TValue, SLOT_POINTER>, Internal::BasicIntrusiveList<TValue, SLOT_POINTER>>
+	{
+	// Public aliases.
+	public:
+		using typename Internal::BasicIntrusiveList<TValue, SLOT_POINTER>::Iterator;
+		using typename Internal::BasicIntrusiveList<TValue, SLOT_POINTER>::ConstIterator;
+
+
+		using Internal::BasicIntrusiveList<TValue, SLOT_POINTER>::IsEmpty;
+
+	// Friendship interface.
+	public:
+		// 'Range-based for loop' interface, the `begin( __range )` statement.
+		friend inline Iterator begin( IntrusiveList& range )						{ return range.GetBegin(); };
+
+		// 'Range-based for loop' interface, the `end( __range )` statement.
+		friend inline Iterator end( IntrusiveList& range )							{ return range.GetEnd(); };
+
+		// 'Range-based for loop' interface, the `begin( __range )` statement.
+		friend inline ConstIterator begin( const IntrusiveList& range )				{ return range.GetBegin(); };
+
+		// 'Range-based for loop' interface, the `end( __range )` statement.
+		friend inline ConstIterator end( const IntrusiveList& range )				{ return range.GetEnd(); };
+
+		// 'Swap' interface.
+		friend inline void swap( IntrusiveList& left, IntrusiveList& right )		{ left.Swap( right ); };
+
+	// Public lifetime management.
+	public:
+		using Black::StandardListFacade<
+			IntrusiveList<TValue, SLOT_POINTER>,
+			Internal::BasicIntrusiveList<TValue, SLOT_POINTER>
+		>::StandardListFacade;
+
+	// Public interface.
+	public:
+		inline explicit operator const bool () const	{ return !IsEmpty(); };
+		inline const bool operator ! () const			{ return IsEmpty(); };
+	};
+}
+}
+}
