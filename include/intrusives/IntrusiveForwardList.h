6d492d2642a7f0eb7366eaaad3a12d888cbdea7c
Frank Stein <dr.frank.stain@gmail.com>
2022-08-15 12:52:38 +0300

    Forward list refactored.

--- include/intrusives/IntrusiveForwardList.h
+++ include/intrusives/IntrusiveForwardList.h
@@ -19,14 +19,26 @@ inline namespace Intrusives
 		@tparam	TValue			Type of stored values.
 		@tparam	SLOT_POINTER	Pointer-to-member of slot to be used.
 	*/
+	template< typename TValue, auto SLOT_POINTER >
+	class IntrusiveForwardList;
+
+	/**
+		@brief	Partial specialization of intrusive forward list, where the slot is placed directly at type of container element.
+
+		@tparam	TValue			Type of stored values.
+		@tparam	SLOT_POINTER	Pointer-to-member of slot to be used.
+	*/
 	template< typename TValue, Black::IntrusiveForwardListSlot TValue::* SLOT_POINTER >
-	class IntrusiveForwardList final
-		: public Black::StandardForwardListFacade<IntrusiveForwardList<TValue, SLOT_POINTER>, Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>>
+	class IntrusiveForwardList<TValue, SLOT_POINTER> final
+		: public Black::StandardForwardListFacade<
+			IntrusiveForwardList<TValue, SLOT_POINTER>,
+			Internal::BasicIntrusiveForwardList<TValue, Internal::IntrusiveTypeTraits<TValue, SLOT_POINTER>>,
+			false
+		>
 	{
 	// Public aliases.
 	public:
 		using typename Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::Iterator;
-		using typename Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::ConstIterator;
 
 
 		using Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::IsEmpty;
@@ -34,16 +46,10 @@ inline namespace Intrusives
 	// Friendship interface.
 	public:
 		// 'Range-based for loop' interface, the `begin( __range )` statement.
-		friend inline Iterator begin( IntrusiveForwardList& range )							{ return range.GetBegin(); };
+		friend inline Iterator begin( const IntrusiveForwardList& range )					{ return range.GetBegin(); };
 
 		// 'Range-based for loop' interface, the `end( __range )` statement.
-		friend inline Iterator end( IntrusiveForwardList& range )							{ return range.GetEnd(); };
-
-		// 'Range-based for loop' interface, the `begin( __range )` statement.
-		friend inline ConstIterator begin( const IntrusiveForwardList& range )				{ return range.GetBegin(); };
-
-		// 'Range-based for loop' interface, the `end( __range )` statement.
-		friend inline ConstIterator end( const IntrusiveForwardList& range )				{ return range.GetEnd(); };
+		friend inline Iterator end( const IntrusiveForwardList& range )						{ return range.GetEnd(); };
 
 		// 'Swap' interface.
 		friend inline void swap( IntrusiveForwardList& left, IntrusiveForwardList& right )	{ left.Swap( right ); };
@@ -52,7 +58,65 @@ inline namespace Intrusives
 	public:
 		using Black::StandardForwardListFacade<
 			IntrusiveForwardList<TValue, SLOT_POINTER>,
-			Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>
+			Internal::BasicIntrusiveForwardList<TValue, Internal::IntrusiveTypeTraits<TValue, SLOT_POINTER>>,
+			false
+		>::StandardForwardListFacade;
+
+
+		inline IntrusiveForwardList& operator = ( IntrusiveForwardList&& other ) noexcept
+		{
+			// End the lifetime of current list.
+			IntrusiveForwardList::~IntrusiveForwardList();
+
+			// Start the lifetime of new list by moving the given `other`.
+			return *new( this ) IntrusiveForwardList{ std::move( other ) };
+		}
+
+	// Public interface.
+	public:
+		inline explicit operator const bool () const	{ return !IsEmpty(); };
+		inline const bool operator ! () const			{ return IsEmpty(); };
+	};
+
+	/**
+		@brief	Partial specialization of intrusive forward list, where the slot is placed at the base type of container element.
+
+		@tparam	TValue			Type of stored values.
+		@tparam	TValueBase		Type of value base, where the container slot is placed.
+		@tparam	SLOT_POINTER	Pointer-to-member of slot to be used.
+	*/
+	template< typename TValue, typename TValueBase, Black::IntrusiveForwardListSlot TValueBase::* SLOT_POINTER >
+	class IntrusiveForwardList<TValue, SLOT_POINTER> final
+		: public Black::StandardForwardListFacade<
+			IntrusiveForwardList<TValue, SLOT_POINTER>,
+			Internal::BasicIntrusiveForwardList<TValue, Internal::IntrusiveTypeTraits<TValue, SLOT_POINTER>>,
+			false
+		>
+	{
+	// Public aliases.
+	public:
+		using typename Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::Iterator;
+
+
+		using Internal::BasicIntrusiveForwardList<TValue, SLOT_POINTER>::IsEmpty;
+
+	// Friendship interface.
+	public:
+		// 'Range-based for loop' interface, the `begin( __range )` statement.
+		friend inline Iterator begin( const IntrusiveForwardList& range )					{ return range.GetBegin(); };
+
+		// 'Range-based for loop' interface, the `end( __range )` statement.
+		friend inline Iterator end( const IntrusiveForwardList& range )						{ return range.GetEnd(); };
+
+		// 'Swap' interface.
+		friend inline void swap( IntrusiveForwardList& left, IntrusiveForwardList& right )	{ left.Swap( right ); };
+
+	// Public lifetime management.
+	public:
+		using Black::StandardForwardListFacade<
+			IntrusiveForwardList<TValue, SLOT_POINTER>,
+			Internal::BasicIntrusiveForwardList<TValue, Internal::IntrusiveTypeTraits<TValue, SLOT_POINTER>>,
+			false
 		>::StandardForwardListFacade;
 
 
