2bdc0ac4d8ac82656cf6f0ab6b1119f821eef654
Frank Stein <dr.frank.stain@gmail.com>
2023-03-28 23:49:51 +0300

    New macro for platform-specific header path used.

--- include/platform/platform.h
+++ include/platform/platform.h
@@ -24,20 +24,8 @@ namespace Internal
 #include "platform-specific/formatting.h"
 #include "platform-specific/memory.h"
 
-// Select the implementation of platform-specific code.
-#if( BLACK_WINDOWS_DESKTOP_PLATFORM )
-	#include "../platform.windows/platform.windows.h"
-#elif( BLACK_MAC_OS_PLATFORM )
-	#include "../platform.macos/platform.macos.h"
-#elif( BLACK_LINUX_PLATFORM )
-	#include "../platform.linux/platform.linux.h"
-#elif( BLACK_ANDROID_PLATFORM )
-	#include "../platform.android/platform.android.h"
-#elif( BLACK_IOS_PLATFORM )
-	#include "../platform.ios/platform.ios.h"
-#else
-	#error Current platform is unspecified or not defined
-#endif
+// Include the platform-specific code.
+#include BLACK_CURRENT_PLATFORM_HEADER
 
 // Deferred implementations.
 #include "functions.inl"
