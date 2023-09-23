f401ab31b83476c3eb8c914dd88ee6460f7596cc
Frank Stein <dr.frank.stain@gmail.com>
2023-09-23 19:12:30 +0300

    Plaform-specific headers in new location.

---
+++ include/.global/platform.windows/platform.windows.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+// External dependencies.
+#include <windows.h>
+
+// WinAPI macro moderation.
+#include "drop-forbidden-macros.h"
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace Global
+{
+inline namespace Platform
+{
+// Windows desktop platform definitions.
+inline namespace WindowsDesktop
+{
+
+}
+}
+}
+}
+}
+
+
+// Common platform-related stuff.
+#include "internal/ComObjectReleaser.h"
+#include "com-pointers.h"
