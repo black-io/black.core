706ec55504ac3f5c0b395f61cf03c876cd393be4
Frank Stein <dr.frank.stain@gmail.com>
2023-12-10 21:55:56 +0300

    Declarations of platform-specific functions for Windows desktop platform.

---
+++ include/.global/platform.windows/functions.memory.windows.h
@@ -0,0 +1,84 @@
+#pragma once
+
+
+namespace Black
+{
+inline namespace Core
+{
+inline namespace Global
+{
+namespace Platform
+{
+	/**
+		@brief	Copies memory from source pointer to destination pointer in terms of Windows desktop platform.
+
+		May cause potentially unsafe behavior if invalid arguments passed.
+
+		@note	The implementation is platform-specific.
+
+		@param	source_memory	Pointer to source memory with `bytes_to_copy` bytes length or higher.
+		@aram	dest_memory		Pointer to destination memory with `bytes_to_copy` bytes length or higher.
+		@param	length			Number of bytes to be copied.
+	*/
+	template<>
+	void CopyMemory<Black::PlatformType::WindowsDesktop>( void* dest_memory, const void* source_memory, const size_t length );
+
+	/**
+		@brief	Fill every byte of `memory` with `pattern` in terms of Windows desktop platform.
+
+		May cause potentially unsafe behavior if invalid arguments passed.
+
+		@note	The implementation is platform-specific.
+
+		@param	memory	Head of memory to be filled.
+		@param	pattern	Filling pattern.
+		@param	length	Number of bytes in `memory` to be filled.
+	*/
+	template<>
+	void FillMemory<Black::PlatformType::WindowsDesktop>( void* memory, const int32_t pattern, const size_t length );
+
+	/**
+		@brief	Fill every byte of `memory` with zero value in terms of Windows desktop platform.
+
+		May cause potentially unsafe behavior if invalid arguments passed.
+
+		@note	The implementation is platform-specific.
+
+		@param	memory	Head of memory to be filled.
+		@param	length	Number of bytes in `memory` to be filled.
+	*/
+	template<>
+	void ZeroMemory<Black::PlatformType::WindowsDesktop>( void* memory, const size_t length );
+
+	/**
+		@brief	Fill every byte of `memory` with zero value in terms of Windows desktop platform.
+
+		May cause potentially unsafe behavior if invalid arguments passed.
+
+		@note	The implementation is platform-specific.
+
+		@note	This function is same as `ZeroMemory`, but it may not be dropped while compiler optimizations.
+		@param	memory	Head of memory to be filled.
+		@param	length	Number of bytes in `memory` to be filled.
+	*/
+	template<>
+	void SecuredZeroMemory<Black::PlatformType::WindowsDesktop>( void* memory, const size_t length );
+
+	/**
+		@brief	Compare the two memory blocks checking its identity in terms of Windows desktop platform.
+
+		May cause potentially unsafe behavior if invalid arguments passed.
+
+		@note	The implementation is platform-specific.
+
+		@param	left_memory		Memory head to compare.
+		@param	right_memory	Memory head to compare.
+		@param	length			The length in bytes to compare.
+		@return					The value returned is flag that the left and right memory streams is equal.
+	*/
+	template<>
+	const bool IsMemoryEqual<Black::PlatformType::WindowsDesktop>( const void* left_memory, const void* right_memory, const size_t length );
+}
+}
+}
+}
