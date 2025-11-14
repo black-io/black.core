646d89b5b753f0d9af0154da4f8f602f9e92330d
Frank Stein <dr.frank.stain@gmail.com>
2025-11-14 16:38:22 +0300

    First implementation of ring buffer type.

--- include/.global/types/RingBuffer.h
+++ include/.global/types/RingBuffer.h
@@ -9,7 +9,132 @@ inline namespace Global
 {
 inline namespace Types
 {
+	/**
+		@brief	Implementation of regular ring buffer.
 
+		AKA circular buffer, circular queue, cyclic buffer.
+		Overview: https://en.wikipedia.org/wiki/Circular_buffer
+
+		This buffer does not own or control the underlying memory buffer. It just organize the data in buffer.
+		Each time the buffer is re-initialized, the data stored before initialization considered lost.
+		Ring buffer can't be copied, but may be moved or invalidated using default-initialized other object.
+	*/
+	class RingBuffer final : private Black::NonCopyable
+	{
+	// Friendship declarations.
+	public:
+		// Allow swapping of buffers.
+		friend void swap( RingBuffer& left, RingBuffer& right ) { left.Swap( right ); };
+
+	// Public lifetime management.
+	public:
+		RingBuffer() noexcept;
+		RingBuffer( RingBuffer&& other ) noexcept;
+		~RingBuffer() noexcept;
+
+		explicit RingBuffer( Black::PlainView<std::byte> buffer ) noexcept;
+
+
+		RingBuffer& operator = ( RingBuffer&& other ) noexcept;
+
+	// Public interface.
+	public:
+		// Rewind the buffer. All previously stored data will be lost.
+		void Rewind();
+
+		// Swap the state of this buffer with other buffer.
+		void Swap( RingBuffer& other );
+
+
+		/**
+			@brief	Store the data from given `buffer` with given `length`.
+
+			@param	buffer			The source buffer with data to be stored.
+			@param	buffer_length	Length of given `buffer`.
+			@return					The value returned is the size of stored data.
+		*/
+		const size_t WriteBuffer( Black::NotNull<const void*> buffer, const size_t buffer_length );
+
+		/**
+			@brief	Store the data from given `buffer`.
+
+			@param	buffer	The source buffer with data to be stored.
+			@return			The value returned is partition of `buffer` with data that was stored.
+		*/
+		Black::PlainView<const std::byte> WriteBuffer( Black::PlainView<const std::byte> buffer );
+
+		/**
+			@brief	Feed the given `buffer` of given `length` with stored data.
+
+			@param	buffer			The buffer to be filled with stored data.
+			@param	buffer_length	Length of given `buffer`.
+			@return					The value returned is the length of stored in `buffer` data.
+		*/
+		const size_t ReadBuffer( Black::NotNull<void*> const buffer, const size_t buffer_length ) const;
+
+		/**
+			@brief	Feed the given `buffer` with stored data.
+
+			@param	buffer	The buffer to be filled with stored data.
+			@return			The value returned is partition of `buffer` filled with data.
+		*/
+		Black::PlainView<std::byte> ReadBuffer( Black::PlainView<std::byte> buffer ) const;
+
+
+		// Get the total capacity of buffer.
+		inline const size_t GetCapacity() const	{ return m_buffer.GetLength(); };
+
+		// Get the length of stored data.
+		inline const size_t GetLength() const	{ return ( m_is_wrapped )? GetWrappedLength() : GetStraightLength(); };
+
+
+		// Whether the buffer can read or write the data.
+		inline const bool IsValid() const	{ return !m_buffer.IsEmpty(); };
+
+		// Whether the buffer contains no data.
+		inline const bool IsEmpty() const	{ return !m_is_wrapped && ( m_read_head == m_write_head ); };
+
+		// Whether the buffer totally filled with data.
+		inline const bool IsFull() const	{ return m_is_wrapped && ( m_read_head == m_write_head ); };
+
+		// Whether the data can be readden from buffer.
+		inline const bool CanRead() const	{ return IsValid() && GetLength() > 0; };
+
+		// Whether the data can be stored in buffer.
+		inline const bool CanWrite() const	{ return IsValid() && GetLength() < GetCapacity(); };
+
+	// Private interface.
+	private:
+		// Commit the writing of stored partition with given length.
+		void CommitWriting( const size_t partition_length ) const;
+
+		// Commit the reading of stored partition with given length.
+		void CommitReading( const size_t partition_length ) const;
+
+
+		// Get the length of next contiguous partition for reading.
+		const size_t GetWritePartitionLength() const;
+
+		// Get the length of next contiguous partition for reading.
+		const size_t GetReadPartitionLength() const;
+
+
+		// Get the length of stored data in straight buffer.
+		inline const size_t GetStraightLength() const	{ return m_write_head - m_read_head; };
+
+		// Get the length of stored data in wrapped around buffer.
+		inline const size_t GetWrappedLength() const	{ return m_buffer.GetLength() - ( m_read_head - m_write_head ); };
+
+	// Private state.
+	private:
+		Black::PlainView<std::byte>	m_buffer;	// Buffer to store the data.
+
+	// Private non-state.
+	private:
+		mutable std::byte*	m_write_head	= nullptr;	// Head of next write operation.
+		mutable std::byte*	m_read_head		= nullptr;	// Head of next read operation.
+		mutable bool		m_is_wrapped	= false;	// Whether the data stored in buffer wraps around the end of buffer.
+	};
 }
 }
 }
--- source/.global/types/RingBuffer.cpp
+++ source/.global/types/RingBuffer.cpp
@@ -1,4 +1,4 @@
-#pragma once
+#include <black/core.h>
 
 
 namespace Black
@@ -9,7 +9,162 @@ inline namespace Global
 {
 inline namespace Types
 {
+	RingBuffer::RingBuffer() noexcept = default;
 
+	RingBuffer::RingBuffer( RingBuffer&& other ) noexcept
+		: m_buffer{ std::move( other.m_buffer ) }
+		, m_write_head{ std::exchange( other.m_write_head, nullptr ) }
+		, m_read_head{ std::exchange( other.m_read_head, nullptr ) }
+		, m_is_wrapped{ std::exchange( other.m_is_wrapped, false ) }
+	{
+	}
+
+	RingBuffer::~RingBuffer() noexcept = default;
+
+	RingBuffer::RingBuffer( Black::PlainView<std::byte> buffer ) noexcept
+		: m_buffer{ std::move( buffer ) }
+		, m_write_head{ m_buffer.GetMemory() }
+		, m_read_head{ m_buffer.GetMemory() }
+	{
+	}
+
+	RingBuffer& RingBuffer::operator=( RingBuffer&& other ) noexcept
+	{
+		RingBuffer temporary{ std::move( other ) };
+		temporary.Swap( *this );
+		return *this;
+	}
+
+	void RingBuffer::Rewind()
+	{
+		m_write_head	= m_buffer.GetMemory();
+		m_read_head		= m_buffer.GetMemory();
+		m_is_wrapped	= false;
+	}
+
+	void RingBuffer::Swap( RingBuffer& other )
+	{
+		Black::Swap( m_buffer, other.m_buffer );
+		Black::Swap( m_write_head, other.m_write_head );
+		Black::Swap( m_read_head, other.m_read_head );
+		Black::Swap( m_is_wrapped, other.m_is_wrapped );
+	}
+
+	const size_t RingBuffer::WriteBuffer( Black::NotNull<const void*> buffer, const size_t buffer_length )
+	{
+		size_t written_length = 0;
+		CRET( IsFull(), written_length );
+
+		const std::byte* source_head = reinterpret_cast<const std::byte*>( buffer.Get() );
+		const size_t max_length = std::min( GetCapacity() - GetLength(), buffer_length );
+		while( written_length < max_length )
+		{
+			const size_t partition_length = std::min( GetWritePartitionLength(), max_length );
+			ENSURES_DEBUG( partition_length > 0 );
+
+			Black::CopyMemory( m_write_head, source_head, partition_length );
+			written_length += partition_length;
+
+			source_head += partition_length;
+			CommitWriting( partition_length );
+		}
+
+		return written_length;
+	}
+
+	Black::PlainView<const std::byte> RingBuffer::WriteBuffer( Black::PlainView<const std::byte> buffer )
+	{
+		Black::PlainView<const std::byte> result;
+
+		const size_t written_length = WriteBuffer( buffer.GetMemory(), buffer.GetLength() );
+		CRET( written_length == 0, result );
+
+		result = { buffer.GetMemory(), written_length };
+		return result;
+	}
+
+	const size_t RingBuffer::ReadBuffer( Black::NotNull<void*> const buffer, const size_t buffer_length ) const
+	{
+		size_t readden_length = 0;
+		CRET( IsEmpty(), readden_length );
+
+		std::byte* dest_head = reinterpret_cast<std::byte*>( buffer.Get() );
+		const size_t max_length = std::min( GetLength(), buffer_length );
+		while( readden_length < max_length )
+		{
+			const size_t partition_length = std::min( GetReadPartitionLength(), max_length );
+			ENSURES_DEBUG( partition_length > 0 );
+
+			Black::CopyMemory( dest_head, m_read_head, partition_length );
+			readden_length += partition_length;
+
+			dest_head += partition_length;
+			CommitReading( partition_length );
+		}
+
+		return readden_length;
+	}
+
+	Black::PlainView<std::byte> RingBuffer::ReadBuffer( Black::PlainView<std::byte> buffer ) const
+	{
+		Black::PlainView<std::byte> result;
+
+		const size_t readden_length = ReadBuffer( buffer.GetMemory(), buffer.GetLength() );
+		CRET( readden_length == 0, result );
+
+		result = { buffer.GetMemory(), readden_length };
+		return result;
+	}
+
+	void RingBuffer::CommitWriting( const size_t partition_length ) const
+	{
+		std::byte* new_write_head = m_write_head + partition_length;
+
+		if( m_buffer.IsInside( new_write_head ) )
+		{
+			ENSURES_DEBUG( !m_is_wrapped || ( new_write_head <= m_read_head ) );
+
+			m_write_head = new_write_head;
+			return;
+		}
+
+		ENSURES_DEBUG( !m_is_wrapped && ( new_write_head == m_buffer.GetEnd() ) );
+
+		m_is_wrapped = true;
+		m_write_head = m_buffer.GetMemory();
+	}
+
+	void RingBuffer::CommitReading( const size_t partition_length ) const
+	{
+		std::byte* new_read_head = m_read_head + partition_length;
+
+		if( m_buffer.IsInside( new_read_head ) )
+		{
+			ENSURES_DEBUG( m_is_wrapped || ( new_read_head <= m_write_head ) );
+
+			m_read_head = new_read_head;
+			return;
+		}
+
+		ENSURES_DEBUG( m_is_wrapped && ( new_read_head == m_buffer.GetEnd() ) );
+
+		m_is_wrapped = false;
+		m_read_head = m_buffer.GetMemory();
+	}
+
+	const size_t RingBuffer::GetWritePartitionLength() const
+	{
+		CRET( m_is_wrapped, m_read_head - m_write_head );
+
+		return m_buffer.GetEnd() - m_write_head;
+	}
+
+	const size_t RingBuffer::GetReadPartitionLength() const
+	{
+		CRET( m_is_wrapped, m_buffer.GetEnd() - m_read_head );
+
+		return m_write_head - m_read_head;
+	}
 }
 }
 }
